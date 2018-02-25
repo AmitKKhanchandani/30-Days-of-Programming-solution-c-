576f4ec29dbd9a3dd957c68dfe73ca24dd4fd12f
Amit K <amitsystem@live.com>
2018-02-25 10:37:41 +0530

    Day 10 Binary Numbers Code added | Updated files to c# filename (cs)

---
+++ Day 10 Binary Numbers.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+class Solution {
+
+    static void Main(String[] args) {
+        int n = Convert.ToInt32(Console.ReadLine());
+			int remainder = 0;
+			List<int> result = new List<int>();
+
+			while (n > 0)
+			{
+				remainder = n % 2;
+				n = n / 2;
+				result.Add(remainder);
+			}
+			
+			int counter = 0;
+			int max = 0;
+			var index = 0;
+			result.ForEach(delegate (int r)
+			{
+				if (r == 1)
+				{
+					counter++;
+
+					if (counter > max)
+					{
+						max = counter;
+					}
+				}
+				else
+				{
+					counter = 0;
+				}
+				index++;
+			});
+			
+			Console.WriteLine(max);
+			Console.ReadLine();
+        
+        
+    }
+}
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 <p align="center">
     <a href="https://www.hackerrank.com/amitkk1">
-        <img height=85 src="https://d3keuzeb2crhkn.cloudfront.net/hackerrank/assets/styleguide/logo_wordmark-f5c5eb61ab0a154c3ed9eda24d0b9e31.svg">
+        <img height=85 src="https://raw.githubusercontent.com/AmitKKhanchandani/30-Days-of-Programming-solution-c-/master/hackerrank.svg">
     </a>
     <br>30 Days of Programming solution c# is based on the Hackrank 30 Days of Code Challenges
     <br><a href="http://www.amitk.co.in">www.amitk.co.in</a>
