3a370c4b4fa0e475f5ce0481e2f414cecc4d02d4
Amit K <amitsystem@live.com>
2018-02-25 10:45:26 +0530

    Indentation fixes

--- Day 10 Binary Numbers.cs
+++ Day 10 Binary Numbers.cs
@@ -6,39 +6,39 @@ class Solution {
 
     static void Main(String[] args) {
         int n = Convert.ToInt32(Console.ReadLine());
-			int remainder = 0;
-			List<int> result = new List<int>();
+		int remainder = 0;
+		List<int> result = new List<int>();
 
-			while (n > 0)
+		while (n > 0)
+		{
+			remainder = n % 2;
+			n = n / 2;
+			result.Add(remainder);
+		}
+		
+		int counter = 0;
+		int max = 0;
+		var index = 0;
+		result.ForEach(delegate (int r)
+		{
+			if (r == 1)
 			{
-				remainder = n % 2;
-				n = n / 2;
-				result.Add(remainder);
+				counter++;
+
+				if (counter > max)
+				{
+					max = counter;
+				}
 			}
-			
-			int counter = 0;
-			int max = 0;
-			var index = 0;
-			result.ForEach(delegate (int r)
+			else
 			{
-				if (r == 1)
-				{
-					counter++;
-
-					if (counter > max)
-					{
-						max = counter;
-					}
-				}
-				else
-				{
-					counter = 0;
-				}
-				index++;
-			});
-			
-			Console.WriteLine(max);
-			Console.ReadLine();
+				counter = 0;
+			}
+			index++;
+		});
+		
+		Console.WriteLine(max);
+		Console.ReadLine();
         
         
     }
